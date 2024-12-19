e43165f9e8eed2cce000647bfd7f521fa65fca2f
Timm Ruppert <timm.ruppert@persival.de>
2024-12-19 09:03:11 +0100

    fix TraceFileFactory
    Signed-off-by: Timm Ruppert <timm.ruppert@persival.de>

--- src/tracefile/reader/Reader.cpp
+++ src/tracefile/reader/Reader.cpp
@@ -9,18 +9,19 @@
 #include "osi-utilities/tracefile/reader/TXTHTraceFileReader.h"
 #include "osi-utilities/tracefile/reader/MCAPTraceFileReader.h"
 
-class TraceFileFactory {
-public:
-    static std::unique_ptr<osi3::TraceFileReader> createReader(const std::filesystem::path& path) {
-        if (path.extension().string() == ".osi") {
-            return std::make_unique<osi3::SingleChannelBinaryTraceFileReader>();
-        }
-        if (path.extension().string() == ".mcap") {
-            return std::make_unique<osi3::MCAPTraceFileReader>();
-        }
-        if (path.extension().string() == ".txth") {
-            return std::make_unique<osi3::TXTHTraceFileReader>();
-        }
-        throw std::invalid_argument("Unsupported format: " + path.extension().string());
+namespace osi3 {
+
+std::unique_ptr<osi3::TraceFileReader> TraceFileReaderFactory::createReader(const std::filesystem::path& path) {
+    if (path.extension().string() == ".osi") {
+        return std::make_unique<osi3::SingleChannelBinaryTraceFileReader>();
     }
-};
+    if (path.extension().string() == ".mcap") {
+        return std::make_unique<osi3::MCAPTraceFileReader>();
+    }
+    if (path.extension().string() == ".txth") {
+        return std::make_unique<osi3::TXTHTraceFileReader>();
+    }
+    throw std::invalid_argument("Unsupported format: " + path.extension().string());
+}
+
+}
